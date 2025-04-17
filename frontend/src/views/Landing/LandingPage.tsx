13c4f9582936f3bab8eefb632a83f9b7fa95fec6
Kevin Heuple <37248863+heuplek@users.noreply.github.com>
2025-04-17 10:17:19 -0400

    clean up date pickers. env variable on fe

--- frontend/src/views/Landing/LandingPage.tsx
+++ frontend/src/views/Landing/LandingPage.tsx
@@ -10,12 +10,23 @@ import AboutPage from "../About/About";
 import Select from "../../components/select/Select";
 import { useCalendarContext } from "../../context/calendarContext";
 import { sandbarWindow } from "../../helpers/sandbarWindow";
+import { getLongDate } from "../../helpers/getLongDate";
 
 const LandingPage = () => {
     const [aboutOpen, setAboutOpen] = useState(false);
     const [weekendSandbarWindow, setWeekendSandbarWindow] = useState<String>("");
     const [weekdaySandbarWindow, setWeekdaySandbarWindow] = useState<String>("");
-    const { weekendIdealLow, setWeekendIdealLow, weekdayIdealLow, setWeekdayIdealLow } = useCalendarContext();
+    const [isWeekend, setIsWeekend] = useState(false);
+    const { weekendIdealLow, setWeekendIdealLow, weekdayIdealLow, setWeekdayIdealLow, day, month, year } = useCalendarContext();
+    const dateStr = year + '-' + (month < 10 ? '0' + month : month) + '-' + (day < 10 ? '0' + (day) : day);
+    const longDay = getLongDate(dateStr);
+    useEffect(()=>{
+        if (longDay == "Saturday" || longDay == "Sunday") {
+            setIsWeekend(true);
+        }else{
+            setIsWeekend(false);
+        }
+    }, [longDay])
     const { width } = useWindowSize();
     const isMobile = width < 784;
     useEffect(() => {
@@ -35,20 +46,20 @@ const LandingPage = () => {
 
             <main>
                 <div className="user-inputs-wrapper">
-                    <Card>
+                    {isWeekend && <Card>
                         <div className="user-inputs-content">
                             <h2>Weekend</h2>
                             <Select value={weekendIdealLow} setValue={setWeekendIdealLow} frame="weekend" />
                             <p className="user-inputs-window">Sandbar window: {weekendSandbarWindow}</p>
                         </div>
-                    </Card>
-                    <Card>
+                    </Card>}
+                    {!isWeekend && <Card>
                         <div className="user-inputs-content">
                             <h2>Week</h2>
                             <Select value={weekdayIdealLow} setValue={setWeekdayIdealLow} frame="weekday" />
                             <p className="user-inputs-window">Sandbar window: {weekdaySandbarWindow}</p>
                         </div>
-                    </Card>
+                    </Card>}
                 </div>
                 <div className="landing-page">
                     <LeftDrawerContainer />
